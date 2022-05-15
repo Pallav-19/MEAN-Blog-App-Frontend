2b4f96b41f596a3de66866fba33fa05a53ff711e
Pallav <pallavinvizag@gmail.com>
2022-05-16 00:55:23 +0530

    database connected and collection and record saved

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const Post = require("./models/post");
 
 const app = express();
+mongoose
+  .connect(
+    "mongodb+srv://mean-app:mean-app@pallav.njiyq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("mongoose connected ");
+  })
+  .catch((err) => {
+    console.log(` an error occured : ${err}`);
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,29 +32,23 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  post.save();
   console.log(post);
   res.status(201).json({
-    message: 'Post added successfully'
+    message: "Post added successfully",
   });
 });
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
-    message: "Posts fetched successfully!",
-    posts: posts
+  Post.find().then((foundData) => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: foundData,
+    });
   });
 });
 
